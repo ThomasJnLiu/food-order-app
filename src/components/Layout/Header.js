7b1f08e8f77b94dfabf042fcbb13bcde4a6adf6c
Thomasjnliu <thomasjnliu@gmail.com>
2021-06-23 20:54:04 -0400

    Add meal components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton/HeaderCartButton";
+import classes from "./Header.module.css";
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      {/* cannot use dot notation with css class names that have spaces */}
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="table of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
