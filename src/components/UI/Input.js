a9a6f8413ae3ea1408e0c85a057e0263060a315d
Thomasjnliu <thomasjnliu@gmail.com>
2021-06-25 00:58:32 -0400

    Add form

---
+++ src/components/UI/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      {/* spread operator ensures all key value pairs in input object are added as attributes to input element, ie type:'text' */}
+      {/* makes element configurable from outside component */}
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
